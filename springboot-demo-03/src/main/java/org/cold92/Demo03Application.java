f71d79899c92d6f02c13f60e63bcd7d7b177738e
DonnKeefer <201800995@qq.com>
2020-09-13 21:37:28 +0800

    第五次提交

---
+++ springboot-demo-03/src/main/java/org/cold92/Demo03Application.java
@@ -0,0 +1,13 @@
+package org.cold92;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo03Application.class, args);
+	}
+
+}
